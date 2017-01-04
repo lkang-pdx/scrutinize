9899af8bb23807edb8fb4fb84c66cb60a7543ec0
Luna Kang <luna.kang.ca@gmail.com>
2017-01-04 09:31:34 -0800

    Update Confirmable Migration

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,10 @@ ActiveRecord::Schema.define(version: 20170104165658) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
